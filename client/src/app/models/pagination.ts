8930ef369b098823746d739566fe36779e484fb5
Addo Ahmed <Abdo.Ahmed@E-Jawdah.com>
2020-11-01 18:38:16 +0200

    Client - Angular Basics

---
+++ client/src/app/models/pagination.ts
@@ -0,0 +1,8 @@
+import { from } from 'rxjs';
+import { IProduct } from './product';
+export interface IPagination {
+    pageIndex: number;
+    pageSize: number;
+    count: number;
+    data: IProduct[];
+  }
