7b22f431003eac6ed725da45e70f83df5d35fd2d
Abdelmoniem Selim <Abdelmoniem_Selim@outlook.com>
2021-06-22 17:11:39 +0200

    Pazzazz

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbComponent, BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,20 +12,26 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute:ActivatedRoute) {}
+  constructor(private shopService: ShopService,private activateRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
   loadProduct() {
-    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(
-      (product) => {
-        this.product = product;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    this.shopService
+      .getProduct(+this.activateRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
